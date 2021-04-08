15eb63cbaf0ae80f69db499b00f8a4bb7e574a84
dongdd159 <79952800+dongdd159@users.noreply.github.com>
2021-04-08 22:02:51 +0700

    accessmodifier

---
+++ src/mypack/B.java
@@ -0,0 +1,12 @@
+package mypack;
+import pack.*;
+public class B {
+    public static void main(String args[]){
+
+        A obj = new A();//Compile Time Error
+
+        obj.msg();//Compile Time Error
+
+    }
+
+}
